c6ce89e7ea1ce87d8a80f5ad05d7ffdbf7770b74
syounng <stellaj0522@gmail.com>
2025-06-21 01:25:58 +0900

    feat: Create CSV File

--- selenium_crawler.py
+++ selenium_crawler.py
@@ -9,6 +9,8 @@ import time
 # 경고 메시지 무시
 import warnings
 warnings.filterwarnings("ignore")
+# 데이터 분석을 위한 라이브러리
+import pandas as pd
 
 # keyword를 검색해서 뉴스 제목을 가져오는 함수
 def crawl_naver_news_with_selenium(keyword):
@@ -33,17 +35,28 @@ def crawl_naver_news_with_selenium(keyword):
     time.sleep(2)
 
     # 뉴스 제목을 찾기 위한 선택자 지정
-    titles = driver.find_elements(By.CSS_SELECTOR, ".sds-comps-text")
+    titles = driver.find_elements(By.CSS_SELECTOR, ".sds-comps-text-type-headline1")
 
     # 뉴스 제목과 링크 출력
     # 몇 번째인지(i)
     # 텍스트 내용(title.text)
     # 링크 주소(title.get_attribute('href'))
     for i, title in enumerate(titles, 1):
-        print(f"{i} {title.text}\n{title.get_attribute('href')}")
+        print(f"{title.text}")
+
+    results = []
+    for i, title in enumerate(titles, 1):
+        results.append({
+            "제목": title.text,
+        })
+    print(f"{i} {title.text}\n{title.get_attribute('href')}")
 
     # 브라우저를 닫고 Selenium 세션 종료
     driver.quit()
+
+    # 결과를 DataFrame으로 변환 후 csv로 저장
+    df = pd.DataFrame(results)
+    df.to_csv(f"{keyword}_news.csv", index=False, encoding="utf-8-sig")
 
 if __name__ == "__main__":
     crawl_naver_news_with_selenium("크래프톤 정글")
